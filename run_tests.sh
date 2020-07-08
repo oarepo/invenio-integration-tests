27beccf03dcebe6f652c4f7f572865028cfa47cd
du-cesnet-travis <noreply@cesnet.cz>
2020-07-08 15:38:54 +0200

    psql test 1

--- run_tests.sh
+++ run_tests.sh
@@ -16,6 +16,15 @@ cat .travis-requirements.txt
 echo "invenio shell:"
 invenio shell --simple-prompt -c "app.config"
 
+echo "DB init, create:"
+invenio db init
+invenio db create
+echo "user create:"
+invenio users create -a noreply@cesnet.cz --password 112233
+echo "user test:"
+invenio shell -c 'from invenio_accounts.models import User; User.query.all()'
+invenio shell -c 'from invenio_accounts.models import User; User.query.all()[0].email'
+
 echo "pip freeze"
 pip freeze > upload/requirements-py${TRAVIS_PYTHON_VERSION}-${REQUIREMENTS}.txt
 grep -F -e invenio= -e invenio-base upload/requirements-py${TRAVIS_PYTHON_VERSION}-${REQUIREMENTS}.txt
