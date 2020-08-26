7f6f44a2fc63d9a4c4b20a57edf583ec012b0bd1
Tom Hlava <hlava@cesnet.cz>
2020-08-26 15:53:19 +0200

    git pull added for concurrent jobs

--- scripts/travis-push.sh
+++ scripts/travis-push.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e
+
 DATE=$(date '+%y%m%d-%H%M%S')
 
 git config --global user.name du-cesnet-travis
 git config --global user.email noreply@cesnet.cz
 
 git checkout -q master \
+ && git pull \
  && git add upload/requirements* \
  && git commit -m "travis commit $DATE (build:$TRAVIS_BUILD_NUMBER result:$TRAVIS_TEST_RESULT)" -m "[skip ci]" \
  && git remote add authenticated https://du-cesnet-travis:${GH_TOKEN}@github.com/oarepo/invenio-integration-tests.git > /dev/null 2>&1 \
  && git push authenticated master 
-
