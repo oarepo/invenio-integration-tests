8d1758535bb2bcfac593954225fc5b63551cc8ce
Tom Hlava <hlava@cesnet.cz>
2020-08-28 12:08:18 +0200

    tagging test + trig3

--- scripts/generate_setup.sh
+++ scripts/generate_setup.sh
@@ -8,14 +8,35 @@
 
 set -e
 
-echo "process_reqs.sh"
+echo "generate_setup.sh"
 
-INVENIO_VERSION=$1
+INVENIO_VERSION_LBL=$1
 
-REQ_FILE="upload/requirements-${INVENIO_VERSION}.txt"
+REQ_FILE="upload/requirements-${INVENIO_VERSION_LBL}.txt"
 SETUP_PY='oarepo/setup.py'
+VERSION_PY='oarepo/oarepo/version.py'
+TAG_TXT='oarepo/oarepo/tag.txt'
 
-test -s "$REQ_FILE" || exit 1
+test -s "$REQ_FILE" || { echo "ERR: REQ_FILE not exists or empty"; exit 1; }
+
+# version bump
+INVENIO_VERSION=$(sed -n '/^invenio==/ {s/^invenio==//; p;}' "$REQ_FILE")
+PREV_INVENIO_VERSION=$(sed -n '/^__version__ / {s/^[^"]\+"\([0-9.]\+\)\.[0-9]"$/\1/;p}' "$VERSION_PY")
+if [[ "$PREV_INVENIO_VERSION" == "$INVENIO_VERSION" ]]; then
+  # bump utility probably can't use four-number format, temporarily switched to three-number format without major ver.num.:
+  MAJOR=$(sed -n '/^__version__ / {s/^[^"]\+"\([0-9]\+\)\..*$/\1/;p}' "$VERSION_PY")
+  BUMP_INPUT=$(sed -n '/^__version__ / {s/^[^"]\+"[0-9]\.\([0-9.]\+\)"[^"]*$/\1/;p}' "$VERSION_PY")
+  BUMPED=$(sed -n '/^__version__ / {s/"[0-9]\./"/;p}' "$VERSION_PY" | bump - /dev/null)
+  # and back with major ver.num.:
+  NEWTAG="$MAJOR.$BUMPED"
+else
+  NEWTAG="$INVENIO_VERSION.0"
+fi
+echo "PREV_INVENIO_VERSION:$PREV_INVENIO_VERSION INVENIO_VERSION:$INVENIO_VERSION NEWTAG:$NEWTAG"
+echo "$NEWTAG" > "$TAG_TXT"
+
+# modify version.py
+sed -i "/^__version__ / {s/\"[0-9.]\+\"/\"$NEWTAG\"/}" "$VERSION_PY"
 
 sed -e "s/^\(.*\)$/  '\1',/" ${REQ_FILE} \
  | sed -i '/^install_requires/,/\]/!b;//!d;/^install_requires/r /dev/stdin' ${SETUP_PY}
