514597349d5dab6b44aa1468c62d7cc67da9d39b
Tom Hlava <hlava@cesnet.cz>
2020-12-09 02:22:49 +0100

    fixes for loop prevention and avoiding unnecessary trigger action

--- scripts/push_back.sh
+++ scripts/push_back.sh
@@ -19,17 +19,34 @@ git clone -q -b "$BRANCH" --depth 10 "$URL" "$DIR"
 cp "$REQFILE" $DIR/upload
 cd "$DIR"
 git add "$REQFILE"
-# only if not devel variant:
+# only for non-devel variant:
 if [[ ! "$REQUIREMENTS" =~ ^devel ]]; then
-  # if test variant => both variants finished => fire trigger:
-  if [[ "$REQUIREMENTS" =~ test$ ]]; then
-    echo "TRIG $(date '+%y%m%d-%H%M%S%z')" > "$TRIG"
-    git add "$TRIG"
+  # if regular non-test variant:
+  if [[ ! "$REQUIREMENTS" =~ test$ ]]; then
+    # something to commit => save the flag:
+    if ! git diff-index --quiet HEAD -- ; then
+      FLG="upload/p2oarepo_action-${REQUIREMENTS}-$GITHUB_RUN_NUMBER.flg"
+      echo "FLG $(date '+%y%m%d-%H%M%S%z')" > "$FLG"
+      git add "$FLG"
+    fi
+  # test variant:
+  else
+    REQS_NOTEST=$(sed 's/test$//' <<<"$REQUIREMENTS")
+    FLG="upload/p2oarepo_action-${REQS_NOTEST}-$GITHUB_RUN_NUMBER.flg"
+    # if something to commit or flag from non-test variant exists:
+    if ! git diff-index --quiet HEAD -- || [[ -f "$FLG" ]]; then
+      # remove the flag
+      [[ -f "$FLG" ]] && git rm "$FLG"
+      # save the trigger
+      echo "TRIG $(date '+%y%m%d-%H%M%S%z')" > "$TRIG"
+      git add "$TRIG"
+    fi
   fi
 fi
-git diff-index --quiet HEAD -- || {
+# if something to commit:
+if ! git diff-index --quiet HEAD -- ; then
   git commit -m "GH action commit $DATE (build:$GITHUB_RUN_NUMBER)" -m "[skip ci]"
   git push origin "$BRANCH"
-}
+fi
 
 echo "Done: $?"
