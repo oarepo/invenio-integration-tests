24f42b78fab6a6f0da50a3fb8e4b16a36b4342cf
Tom Hlava <hlava@cesnet.cz>
2020-12-18 19:33:36 +0100

    GH action trigger 201218-193336

--- scripts/generate_setup.sh
+++ scripts/generate_setup.sh
@@ -26,7 +26,7 @@ pip install bump
 # 1st two digits of fixed invenio version from the requirements file:
 INVENIO_VERSION=$(sed -n '/^invenio==/ {s/^invenio==\([0-9]\+\.[0-9]\+\)\(\.[0-9]\+\)\?$/\1/; p;}' "$REQ_FILE")
 # 1st two digits of previous version from version.py:
-PREV_INVENIO_VERSION=$(sed -n '/^__version__ / {s/^[^"\x27]\+["\x27]\([0-9]\+\.[0-9]\)\(\.[0-9]\+\)\?["\x27]$/\1/;p}' "$VERSION_PY")
+PREV_INVENIO_VERSION=$(sed -n '/^__version__ / {s/^[^"\x27]\+["\x27]\([0-9]\+\.[0-9]\)\(\.[0-9]\+\)*["\x27]$/\1/;p}' "$VERSION_PY")
 
 # if there is new invenio version, update 1st two numbers accordingly:
 if [[ "$PREV_INVENIO_VERSION" != "$INVENIO_VERSION" ]]; then
