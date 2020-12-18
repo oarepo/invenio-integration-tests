91076c74426e52e24aa0e6ce00ccd7355a871942
Tom Hlava <hlava@cesnet.cz>
2020-12-18 19:48:18 +0100

    bumping fixed + action trigger
    [skip ci]

--- scripts/generate_setup.sh
+++ scripts/generate_setup.sh
@@ -32,12 +32,16 @@ PREV_INVENIO_VERSION=$(sed -n '/^__version__ / {s/^[^"\x27]\+["\x27]\([0-9]\+\.[
 if [[ "$PREV_INVENIO_VERSION" != "$INVENIO_VERSION" ]]; then
   sed -i "/^__version__ / {s/[\"'][0-9.]\+[\"']/\"$INVENIO_VERSION\"/}" "$VERSION_PY"
 fi
-# bump version.py + catch new value:
-NEWTAG=$(bump "$VERSION_PY" "$VERSION_PY")
+# bump version.py (ignore 4th number) + catch new value:
+NEWTAG=$(sed -n '/^__version__ / {s/\.[0-9]"/"/;p}' "$VERSION_PY" | bump - /dev/null)
 
 echo "PREV_INVENIO_VERSION:$PREV_INVENIO_VERSION INVENIO_VERSION:$INVENIO_VERSION NEWTAG:$NEWTAG"
 echo "$NEWTAG" > "$TAG_TXT"
 
+# modify version.py:
+sed -i "/^__version__ / {s/\"[0-9.]\+\"/\"$NEWTAG\"/}" "$VERSION_PY"
+
+# modify setup.py:
 sed -e "s/^\(.*\)\$/    '\1',/" ${REQ_FILE} \
  | sed -i '/^install_requires/,/^\]/!b;//!d;/^install_requires/r /dev/stdin' ${SETUP_PY}
 sed -e '/pytest-invenio/ s/^pytest-invenio==\([0-9\.]\+\)$/pytest-invenio[docs]==\1/' -e "s/^\(.*\)\$/        '\1',/" ${REQ_FILE_TEST} \
