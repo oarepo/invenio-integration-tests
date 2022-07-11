234bf3902661bad56d5446bba69dfaf3b87b91ac
Tom Hlava <hlava@cesnet.cz>
2022-07-11 18:59:43 +0200

    rdm-9 support 6

--- scripts/generate_oarepo_setup.sh
+++ scripts/generate_oarepo_setup.sh
@@ -10,10 +10,10 @@ set -e
 
 echo "generate_oarepo_setup.sh"
 
-INVENIO_VERSION_LBL=$1
+LBL=$1
 
-REQ_FILE="upload/requirements-${INVENIO_VERSION_LBL}.txt"
-REQ_FILE_TEST="upload/requirements-${INVENIO_VERSION_LBL}-test.txt"
+REQ_FILE="upload/requirements-${LBL}.txt"
+REQ_FILE_TEST="upload/requirements-${LBL}-test.txt"
 SETUP_PY='oarepo/setup.py'
 VERSION_PY='oarepo/oarepo/version.py'
 TAG_TXT='oarepo/oarepo/tag.txt'
@@ -24,24 +24,24 @@ python -m pip install --upgrade pip
 pip install bump
 
 # 1st two digits of fixed invenio version from the requirements file:
-echo -n "INVENIO_VERSION:"
-INVENIO_VERSION=$(sed -n '/^invenio==/ {s/^invenio==\([0-9]\+\.[0-9]\+\)\(\.[0-9]\+\)\?$/\1/; p;}' "$REQ_FILE")
-echo $INVENIO_VERSION
+#echo -n "INVENIO_VERSION:"
+#INVENIO_VERSION=$(sed -n '/^invenio==/ {s/^invenio==\([0-9]\+\.[0-9]\+\)\(\.[0-9]\+\)\?$/\1/; p;}' "$REQ_FILE")
+#echo $INVENIO_VERSION
 # 1st two digits of previous version from version.py:
-echo -n "PREV_INVENIO_VERSION:"
-PREV_INVENIO_VERSION=$(sed -n '/^__version__ / {s/^[^"\x27]\+["\x27]\([0-9]\+\.[0-9]\)\(\.[0-9]\+\)*["\x27]$/\1/;p}' "$VERSION_PY")
-echo $PREV_INVENIO_VERSION
+#echo -n "PREV_INVENIO_VERSION:"
+#PREV_INVENIO_VERSION=$(sed -n '/^__version__ / {s/^[^"\x27]\+["\x27]\([0-9]\+\.[0-9]\)\(\.[0-9]\+\)*["\x27]$/\1/;p}' "$VERSION_PY")
+#echo $PREV_INVENIO_VERSION
 
 # if there is new invenio version, update 1st two numbers accordingly:
-if [[ "$PREV_INVENIO_VERSION" != "$INVENIO_VERSION" ]]; then
-  sed -i "/^__version__ / {s/[\"'][0-9.]\+[\"']/\"$INVENIO_VERSION\"/}" "$VERSION_PY"
-fi
+#if [[ "$PREV_INVENIO_VERSION" != "$INVENIO_VERSION" ]]; then
+#  sed -i "/^__version__ / {s/[\"'][0-9.]\+[\"']/\"$INVENIO_VERSION\"/}" "$VERSION_PY"
+#fi
 # bump version.py (ignore 4th number) + catch new value:
 echo -n "NEWTAG:"
 NEWTAG=$(sed -n '/^__version__ / {s/\.[0-9]"/"/;p}' "$VERSION_PY" | bump - /dev/null)
 echo $NEWTAG
 
-echo "PREV_INVENIO_VERSION:$PREV_INVENIO_VERSION INVENIO_VERSION:$INVENIO_VERSION NEWTAG:$NEWTAG"
+#echo "PREV_INVENIO_VERSION:$PREV_INVENIO_VERSION INVENIO_VERSION:$INVENIO_VERSION NEWTAG:$NEWTAG"
 echo "$NEWTAG" > "$TAG_TXT"
 
 # modify version.py:
