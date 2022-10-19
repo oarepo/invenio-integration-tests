bf05c7d9e9584b1014d7487aeed5fb08eca6637d
Tom Hlava <hlava@cesnet.cz>
2022-10-19 14:45:51 +0200

    rdm-10 init

--- scripts/push_back.sh
+++ scripts/push_back.sh
@@ -13,7 +13,7 @@ URL="https://oarepo-bot:${TOK}@github.com/oarepo/invenio-integration-tests.git"
 DIR="invenio-integration-tests"
 REQFILE="upload/requirements-${REQUIREMENTS}.txt"
 TRIG="upload/p2oarepo_action.trigger"
-BRANCH="rdm-9"
+BRANCH="rdm-10"
 
 git clone -q -b "$BRANCH" --depth 10 "$URL" "$DIR"
 cp "$REQFILE" $DIR/upload
