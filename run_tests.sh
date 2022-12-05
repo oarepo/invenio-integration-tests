df199b3873fe6f9d3e19e6e82077b6d82534070d
Tom Hlava <hlava@cesnet.cz>
2022-12-05 15:22:38 +0100

    rdm-11 test 8, clean up + pushback

--- run_tests.sh
+++ run_tests.sh
@@ -16,13 +16,11 @@ function cleanup(){
 trap cleanup EXIT
 
 ENV0="$(printenv)"
-echo -e "ENV1:\n$ENV0\n"
 
 echo "docker-services-cli up (DB:$DB; SEARCH:$SEARCH)"
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch7} --mq ${MQ:-redis} --env)"
 
 ENV1="$(printenv)"
-echo -e "ENV1:\n$ENV1\n"
 
 echo "env diff:"
 diff <(echo "$ENV0") <(echo "$ENV1") || true
@@ -32,7 +30,7 @@ export INVENIO_JSONSCHEMAS_HOST=repozitar.cesnet.cz
 echo -e "\ninvenio shell, invenio_config's version.__version__):"
 invenio shell --simple-prompt -c "from invenio_config import version; print (\"invenio_config version:\", version.__version__)"
 
-echo -e "SQLALCHEMY_DATABASE_URI:$SQLALCHEMY_DATABASE_URI\nINVENIO_SQLALCHEMY_DATABASE_URI:$INVENIO_SQLALCHEMY_DATABASE_URI"
+echo -e "SQLALCHEMY_DATABASE_URI: $SQLALCHEMY_DATABASE_URI\nINVENIO_SQLALCHEMY_DATABASE_URI: $INVENIO_SQLALCHEMY_DATABASE_URI"
 echo -e "\npsql version:"
 psql --version
 echo "invenio db init,create:"
