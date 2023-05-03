03e0f472690cbf9932a322429fc4f2db7ea43edf
Tom Hlava <hlava@cesnet.cz>
2023-05-03 15:52:30 +0200

    test invenio-base ver.pin

--- setup.py
+++ setup.py
@@ -28,7 +28,8 @@ extras_require = {
         'invenio-oauth2server==1.3.1',
         'pytest-cov==2.10.1',
         'coverage==4.5.4',
-        'pillow>=8.3.2'
+        'pillow>=8.3.2',
+        'invenio-base<1.2.14'
     ],
     'invenio3.3test': [
         'invenio[base,metadata,files,postgresql,elasticsearch7,tests]>=3.3.0,<3.4.0',
@@ -40,7 +41,8 @@ extras_require = {
         'invenio-oauth2server==1.3.1',
         'pytest-cov==2.10.1',
         'coverage==4.5.4',
-        'pillow>=8.3.2'
+        'pillow>=8.3.2',
+        'invenio-base<1.2.14'
     ],
     'devel': [
         'invenio[base,metadata,files,postgresql,elasticsearch7]',
