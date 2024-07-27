1a49fbd44ed7ee85f0ff9b9a4ef15b8f1d5ed211
Mirek Simek <miroslav.simek@gmail.com>
2024-07-27 15:05:07 +0200

    Fixed problematic versions between normal installation and tests

--- scripts/merge_requirements_to_pyproject_toml.py
+++ scripts/merge_requirements_to_pyproject_toml.py
@@ -22,9 +22,9 @@ def main(
     with open(pyproject_toml_path, "r") as f:
         pyproject_toml = toml.load(f)
 
-    normal_requirements = []
-    rdm_requirements = []
-    test_requirements = []
+    normal_requirements = {}
+    rdm_requirements = {}
+    test_requirements = {}
     seen_packages = set()
 
     with open(requirements_path, "r") as f:
@@ -33,28 +33,36 @@ def main(
             if req.name in seen_packages:
                 continue
             if req.name in ('invenio-app-rdm', 'invenio-rdm-records'):
-                rdm_requirements.append(req)
+                rdm_requirements[req.name] = req
             else:
-                normal_requirements.append(req)
+                normal_requirements[req.name] = req
             seen_packages.add(req.name)
 
     with open(test_requirements_path, "r") as f:
         requirements = parse_requirements(f.read())
         for req in requirements:
             if req.name in seen_packages:
+                if req.name in normal_requirements:
+                    # check if the version matches, if not, take the one from tests
+                    if normal_requirements[req.name].line != req.line:
+                        click.secho(f"Version mismatch for {req.name}: "
+                                    f"normal installation {normal_requirements[req.name].line}, in tests {req.line}. "
+                                    f"Taking tests' version.",
+                                    fg="red")
+                        normal_requirements[req.name] = req
                 continue
-            test_requirements.append(req)
+            test_requirements[req.name] = req
             seen_packages.add(req.name)
 
     pyproject_toml["project"]["dependencies"] = [
-        req.line for req in normal_requirements
+        req.line for req in normal_requirements.values()
     ]
     pyproject_toml["project"]["optional-dependencies"] = {
-        "rdm": [req.line for req in rdm_requirements],
-        "test": [req.line for req in test_requirements],
-        "tests": [req.line for req in test_requirements],
-        "dev": [req.line for req in test_requirements],
-        "devs": [req.line for req in test_requirements],
+        "rdm": [req.line for req in rdm_requirements.values()],
+        "test": [req.line for req in test_requirements.values()],
+        "tests": [req.line for req in test_requirements.values()],
+        "dev": [req.line for req in test_requirements.values()],
+        "devs": [req.line for req in test_requirements.values()],
     }
 
     encoder = toml.TomlArraySeparatorEncoder(separator=",\n    ")
