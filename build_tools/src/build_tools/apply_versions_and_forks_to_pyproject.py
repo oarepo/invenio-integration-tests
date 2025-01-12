0c719cdbc8f835312dfc9f071fd675639a75b754
Mirek Simek <miroslav.simek@gmail.com>
2025-01-12 22:17:27 +0100

    Correct package versions

--- build_tools/src/build_tools/apply_versions_and_forks_to_pyproject.py
+++ build_tools/src/build_tools/apply_versions_and_forks_to_pyproject.py
@@ -1,42 +1,56 @@
 import json
-from importlib.metadata import version
 from pathlib import Path
 from pprint import pprint
 from typing import Annotated
-import toml
-import semver
 
+import semver
+import toml
 import typer
 
 app = typer.Typer()
 
-@app.command()
-def main(oarepo_version: str,
-        requirements_json_file: Annotated[Path, typer.Argument(help="Path to the rdm_requirements.json file")],
-        test_requirements_json_file: Annotated[Path, typer.Argument(help="Path to the rdm_test_requirements.json file")],
-        forked_packages_json_file: Annotated[Path, typer.Argument(help="Path to the forked_packages.json file")],
-        pyproject_toml_path: Annotated[Path, typer.Argument(help="Path to the output file")]):
 
+@app.command()
+def main(
+    oarepo_version: str,
+    requirements_json_file: Annotated[
+        Path, typer.Argument(help="Path to the rdm_requirements.json file")
+    ],
+    test_requirements_json_file: Annotated[
+        Path, typer.Argument(help="Path to the rdm_test_requirements.json file")
+    ],
+    forked_packages_json_file: Annotated[
+        Path, typer.Argument(help="Path to the forked_packages.json file")
+    ],
+    final_forked_packages_and_versions: Annotated[
+        Path, typer.Argument(help="Path to the directory with packages and versions")
+    ],
+    pyproject_toml_path: Annotated[
+        Path, typer.Argument(help="Path to the output file")
+    ],
+):
     normal_requirements = json.loads(requirements_json_file.read_text())
     test_requirements = json.loads(test_requirements_json_file.read_text())
     forked_packages = json.loads(forked_packages_json_file.read_text())
 
     # a list of serialized json objects in fact to enable usage in github matrix
     forked_packages = [json.loads(x) for x in forked_packages]
-    forked_packages = {x['package']: x['version'] for x in forked_packages}
+    forked_packages = {x["package"]: x["version"] for x in forked_packages}
 
-    common_versions = {
-        r['name']: r['version'] for r in normal_requirements
-    }
+    for ff in final_forked_packages_and_versions.glob("*.txt"):
+        data = ff.read_text().strip()
+        _package, _version = data.split("==")
+        forked_packages[_package] = _version
+
+    common_versions = {r["name"]: r["version"] for r in normal_requirements}
     for r in test_requirements:
-        name = r['name']
-        version = r['version']
+        name = r["name"]
+        version = r["version"]
         if name not in common_versions:
             common_versions[name] = version
         elif common_versions[name] != version:
             print(f"Version conflict for {name}: {common_versions[name]} vs {version}")
             common_versions[name] = semver.min_ver(common_versions[name], version)
-
 
     with open(pyproject_toml_path, "r") as f:
         pyproject_toml = toml.load(f)
@@ -50,13 +64,13 @@ def main(oarepo_version: str,
     test_dependencies = []
 
     rdm_packages = {
-        'invenio-app-rdm',
-        'invenio-rdm-records',
+        "invenio-app-rdm",
+        "invenio-rdm-records",
     }
 
     normal_requirements_dict = {}
     for r in normal_requirements:
-        formatted, name = format_dependency(forked_packages, r['name'], common_versions)
+        formatted, name = format_dependency(forked_packages, r["name"], common_versions)
         normal_requirements_dict[name] = formatted
 
         if name in rdm_packages:
@@ -65,11 +79,10 @@ def main(oarepo_version: str,
             dependencies.append(formatted)
 
     for r in test_requirements:
-        if r['name'] in normal_requirements_dict:
+        if r["name"] in normal_requirements_dict:
             continue
-        formatted, name = format_dependency(forked_packages, r['name'], common_versions)
+        formatted, name = format_dependency(forked_packages, r["name"], common_versions)
         test_dependencies.append(formatted)
-
 
     pyproject_toml["project"]["dependencies"] = dependencies
 
@@ -79,7 +92,7 @@ def main(oarepo_version: str,
         "tests": test_dependencies,
         "dev": test_dependencies,
         "devs": test_dependencies,
-        "s3": []  # backward compatibility, s3 is baked in the base image
+        "s3": [],  # backward compatibility, s3 is baked in the base image
     }
 
     encoder = toml.TomlArraySeparatorEncoder(separator=",\n    ")
@@ -100,4 +113,3 @@ def format_dependency(forked_packages, name, versions):
 
 if __name__ == "__main__":
     app()
-
