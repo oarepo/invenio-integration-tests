15e1bf5061efaaa1c3d16c096c7e2e5721e64048
Mirek Simek <miroslav.simek@gmail.com>
2025-01-15 09:26:28 +0100

    working on build

--- build_tools/src/build_tools/install_frozen_dependencies.py
+++ build_tools/src/build_tools/install_frozen_dependencies.py
@@ -16,32 +16,17 @@ from typing import Any
 def install_frozen_dependencies(invenio_forks: Path):
     forks = json.loads(invenio_forks.read_text())
     fork_packages: list[Any] = forks.get("packages", [])
-    actual_packages = load_freeze_from_venv()
     packages_to_install: list[str] = []
     for pkg in fork_packages:
         if "frozen" not in pkg:
             continue
-        actual_pkg_version = actual_packages.get(pkg["name"].lower())
         for frozen in pkg["frozen"]:
-            print(
-                f"Checking if should apply frozen version for {pkg["name"]} @ {actual_pkg_version}"
-            )
-            invenio_version = frozen.get("invenio-version", None)
-            print(
-                "Versions of the package that should have the dependencies frozen: ",
-                invenio_version,
-            )
-
-            if not invenio_version or check_pkg_version(
-                actual_pkg_version, invenio_version
-            ):
-                print(
-                    f"Collecting frozen dependencies for {pkg["name"]} @ {actual_pkg_version}"
-                )
-                print(frozen["packages"])
-                packages_to_install.extend(frozen["packages"])
+            packages_to_install.extend(frozen["packages"])
 
     if packages_to_install:
+        print("Installing frozen dependencies:")
+        for pkg in packages_to_install:
+            print(f"  {pkg}")
         subprocess.check_call(["uv", "pip", "install", *packages_to_install])
         print("After installation, the freeze is:")
         subprocess.call(["uv", "pip", "freeze"])
