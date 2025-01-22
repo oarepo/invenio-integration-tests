bde80ea02f4b32abb467805bde5a8b33b928342e
Mirek Simek <miroslav.simek@gmail.com>
2025-01-22 08:18:33 +0100

    fork dependencies

--- build_tools/src/build_tools/fix_versions_in_venv.py
+++ build_tools/src/build_tools/fix_versions_in_venv.py
@@ -1,3 +1,5 @@
+import json
+import os
 import subprocess
 import sys
 from pathlib import Path
@@ -21,19 +23,23 @@ def load_freeze_from_file(path: Path) -> dict[str, str]:
     }
 
 
-def fix_versions(rdm_requirements_file: Path):
+def fix_versions(rdm_requirements_file: Path, this_module_config_file: Path):
     actual_packages = load_freeze_from_venv()
     rdm_packages = load_freeze_from_file(rdm_requirements_file)
 
     packages_to_update: list[str] = []
 
+    versions: dict[str, str] = {}
+
     for pkg_name, pkg_version in actual_packages.items():
+        versions[pkg_name] = pkg_version
         if pkg_name not in rdm_packages or rdm_packages[pkg_name] == pkg_version:
             continue
         if pkg_name in ("importlib_metadata", "importlib-metadata"):
             # invenio-administration's mocks depend on the newer version of importlib_metadata
             continue
         packages_to_update.append(f"{pkg_name}=={rdm_packages[pkg_name]}")
+        versions[pkg_name] = rdm_packages[pkg_name]
 
     if packages_to_update:
         print("Updating packages to be in sync with actual RDM deps:")
@@ -43,6 +49,28 @@ def fix_versions(rdm_requirements_file: Path):
     else:
         print("All packages are in sync with RDM deps.")
 
+    pypi_url = os.environ["CESNET_PYPI_URL"]
+
+    module_config = json.loads(this_module_config_file.read_text())
+    for dependency in module_config.get("depends", []):
+        depends_on = dependency["on"]
+        # this module depends on a patched version of another module
+        if depends_on in versions:
+            version = versions[depends_on]
+            if ".post" not in version:
+                version += ".post*"
+            print(f"Installing patched version of {depends_on} ({version})")
+            subprocess.check_call(
+                [
+                    "uv",
+                    "pip",
+                    "install",
+                    "--extra-index-url",
+                    pypi_url,
+                    f"{depends_on}=={version}",
+                ]
+            )
+
 
 if __name__ == "__main__":
-    fix_versions(Path(sys.argv[1]))
+    fix_versions(Path(sys.argv[1]), Path(sys.argv[2]))
