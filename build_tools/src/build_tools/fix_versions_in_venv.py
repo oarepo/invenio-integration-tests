dd1d8652021480536d74772a0dfe2b847a3edba0
Mirek Simek <miroslav.simek@gmail.com>
2025-01-22 13:58:19 +0100

    Trying to install correct package version

--- build_tools/src/build_tools/fix_versions_in_venv.py
+++ build_tools/src/build_tools/fix_versions_in_venv.py
@@ -60,7 +60,24 @@ def fix_versions(rdm_requirements_file: Path, this_module_config_file: Path):
             if ".post" not in version:
                 version += ".post1000000"
             print(f"Installing patched version of {depends_on} ({version})")
-            subprocess.call(["uv", "pip", "uninstall", "-y", depends_on])
+            print("Calling", ["uv", "pip", "uninstall", "--verbose", "-y", depends_on])
+            subprocess.check_call(
+                ["uv", "pip", "uninstall", "--verbose", "-y", depends_on]
+            )
+            print(
+                "Calling",
+                [
+                    "uv",
+                    "pip",
+                    "install",
+                    "-U",
+                    "--no-deps",
+                    "--verbose",
+                    "--extra-index-url",
+                    pypi_url,
+                    f"{depends_on}<={version}",
+                ],
+            )
             subprocess.check_call(
                 [
                     "uv",
