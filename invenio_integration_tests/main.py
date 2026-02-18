92908f672e339eb3019bd31a96c8b0cb6269c361
Miroslav Simek <3385642+mesemus@users.noreply.github.com>
2026-02-18 20:27:10 +0100

    working on new integration tests

---
+++ invenio_integration_tests/main.py
@@ -0,0 +1,79 @@
+import json
+from pathlib import Path
+
+import click
+
+
+@click.group()
+def cli():
+    """Invenio Integration Tests CLI."""
+    pass
+
+
+@cli.command("setup")
+@click.argument(
+    "input_config", type=click.Path(path_type=Path, resolve_path=True, exists=True)
+)
+@click.argument(
+    "preprocessed_config", type=click.Path(path_type=Path, resolve_path=True)
+)
+def setup(input_config: Path, preprocessed_config: Path):
+    """Preprocess the input configuration and save it to the preprocessed configuration path."""
+    import yaml
+
+    with input_config.open() as f:
+        config = yaml.safe_load(f)
+
+    packages = config.pop("packages", [])
+    supported_invenio_version = config.pop("invenio-version", None)
+    patches = []
+    entrypoints = {}
+    for package_def in packages:
+        package_name = package_def.pop("name")
+        _unused_depends = package_def.pop("depends", [])
+        _unused_dependencies = package_def.pop("dependencies", [])
+        _unused_tests = package_def.pop("tests", [])
+        if not package_name:
+            raise ValueError(
+                f"Package definition must include a 'name' field, got {package_def}"
+            )
+        for feature in package_def.pop("features", []):
+            if isinstance(feature, str):
+                patches.append(feature)
+                continue
+            feature_name = feature.pop("name")
+            feature_base = feature.pop("base")
+            feature_str = (
+                f"oarepo/{package_name}@oarepo-feature-{feature_name}[{feature_base}]"
+            )
+
+            invenio_version = feature.pop("invenio-version", None)
+            if invenio_version:
+                feature_str += invenio_version
+
+            patches.append(feature_str)
+            assert not feature, f"Unexpected keys in feature definition: {feature}"
+
+        package_entrypoints = package_def.pop("entrypoints", {})
+        if package_entrypoints:
+            entrypoints[package_name] = package_entrypoints
+
+        assert not package_def, f"Unexpected keys in package definition: {package_def}"
+
+    assert not config, f"Unexpected keys in configuration: {config}"
+
+    with preprocessed_config.open("w") as f:
+        json.dump(
+            {
+                "invenio-version": supported_invenio_version,
+                "patches": patches,
+                "entrypoints": entrypoints,
+            },
+            f,
+            indent=2,
+            ensure_ascii=False,
+        )
+
+
+if __name__ == "__main__":
+    cli()
--- run_locally.sh
+++ run_locally.sh
@@ -1,90 +1,26 @@
-#!/bin/bash
-
-# TODO: read environment vars from .github/workflows/main.yml
-OAREPO_VERSION=12
-export OAREPO_VERSION
-
-APP_RDM_PRODUCTION_VERSION="invenio-app-rdm[opensearch2,s3]>=12.0.0b2.dev1,<13"
-APP_RDM_TEST_VERSION="invenio-app-rdm[opensearch2,s3,tests]>=12.0.0b2.dev1,<13"
-export APP_RDM_PRODUCTION_VERSION
-export APP_RDM_TEST_VERSION
-
-
-PYTHON="python3.10"
-export PYTHON
-
-# END OF TODO
+#!/usr/bin/env bash
 
 set -e
+set -o pipefail
+set -u
 
 cd "$(dirname "$0")"
 
-EXCLUDED_STEPS=( "Test opensearch on port" "Test postgres version" )
+# if [ -d .venv ]; then
+#     rm -rf .venv
+# fi
+# uv venv --python=3.14
+# source .venv/bin/activate
+# uv pip install -e .
 
-IFS=$'\n'
-STEPS=( $(cat .github/workflows/main.yml | yq -o=j -I=0 '.jobs.start_and_test.steps[]') )
+invenio-integration-tests setup invenio-forks.yaml test.json
+# create --patch arguments from test.json
 
-GITHUB_WORKSPACE="$(pwd)/.workspace"
-export GITHUB_WORKSPACE
+patches=()
+while IFS= read -r patch; do
+  patches+=(--patch "$patch")
+done < <(jq -r '.patches[]' test.json)
 
-GITHUB_ENV="$GITHUB_WORKSPACE/.github_env"
-export GITHUB_ENV
 
-GITHUB_OUTPUT="$GITHUB_WORKSPACE/.github_output"
-export GITHUB_OUTPUT
-
-if [ -d "$GITHUB_WORKSPACE" ]; then
-  rm -rf "$GITHUB_WORKSPACE"
-fi
-
-mkdir "$GITHUB_WORKSPACE"
-
-ls | while read; do
-  if [ "$REPLY" != ".workspace" ]; then
-    cp -r "$REPLY" "$GITHUB_WORKSPACE"
-  fi
-done
-
-cd $GITHUB_WORKSPACE
-
-for step in "${STEPS[@]}"; do
-  step_name=$(echo "$step" | jq -r '.name')
-  step_uses=$(echo "$step" | jq -r '.uses')
-  step_with=$(echo "$step" | jq -r '.with')
-  step_run=$(echo "$step" | jq -r '.run')
-  SKIPPED=""
-  for excluded_step in "${EXCLUDED_STEPS[@]}"; do
-    if [[ "$step_name" =~ $excluded_step ]]; then
-      SKIPPED="true"
-    fi
-  done
-  if [ "$SKIPPED" == "true" ]; then
-    echo "skipping step: $step_name"
-    continue
-  fi
-  echo "==========================================="
-  echo ""
-  echo ""
-  echo "Running step: '$step_name'"
-  echo ""
-  echo ""
-  if [ "$step_uses" == "actions/checkout@v4" ] ; then
-    if [ "$step_with" != "null" ]; then
-      repository=$(echo "$step_with" | jq -r '.repository')
-      ref=$(echo "$step_with" | jq -r '.ref' | sed "s/\\\${{ env\\.OAREPO_VERSION }}/${OAREPO_VERSION}/")
-      path=$(echo "$step_with" | jq -r '.path')
-      echo "cloning repository: '$repository' ref: '$ref' path: '$path'"
-      git clone --depth=1 --branch="$ref" https://github.com/"$repository" "$GITHUB_WORKSPACE/$path"
-    fi
-  fi
-  if [ "$step_run" != "null" ] ; then
-    echo "----------------------------------------"
-    echo "$step_run" | sed 's/^/  > /'
-    {
-      cd "$GITHUB_WORKSPACE"
-      eval "$step_run"
-      cd "$GITHUB_WORKSPACE"
-    }
-    echo "----------------------------------------"
-  fi
-done
+invenio-testrig setup --verbose  --repository oarepo/inveniordm-reference-repo \
+  --patch-mode pinned-rebase "${patches[@]}" 
