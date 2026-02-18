9991be9889882d54b4d11d1090707201b724f3d3
Miroslav Simek <3385642+mesemus@users.noreply.github.com>
2026-02-18 22:45:25 +0100

    Adding correct patches

--- invenio_integration_tests/main.py
+++ invenio_integration_tests/main.py
@@ -43,7 +43,8 @@ def setup(input_config: Path, preprocessed_config: Path):
                 continue
             feature_name = feature.pop("name")
             feature_base = feature.pop("base")
-            feature_str = f"oarepo/{package_name}@{feature_name}[{feature_base}]"
+            feature_org = feature.pop("org", "oarepo")
+            feature_str = f"{feature_org}/{package_name}@{feature_name}[{feature_base}]"
 
             invenio_version = feature.pop("invenio-version", None)
             if invenio_version:
